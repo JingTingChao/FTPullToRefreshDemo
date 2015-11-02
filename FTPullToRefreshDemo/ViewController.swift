27b33d56477106a4d79f4ecf52ddc6b1b90486c5
futantan <luckytantanfu@gmail.com>
2015-11-02 18:44:33 +0800

    init project

--- FTPullToRefreshDemo/ViewController.swift
+++ FTPullToRefreshDemo/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private let ReusableCellIdentifier = "Cell"
+
+class ViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController {
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier(ReusableCellIdentifier, forIndexPath: indexPath)
+    cell.textLabel?.text = "item \(indexPath.row)"
+    
+    return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ViewController {
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+  }
+}
